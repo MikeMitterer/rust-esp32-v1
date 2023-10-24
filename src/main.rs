96816cd9bc4eb8fe1446f6e97b3e5755f0478df8
Mike Mitterer <office@mikemitterer.at>
2023-10-24 19:29:04 +0200

    DrawTarget not implemented

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,73 @@
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 use log::*;
 
-fn main() {
+use esp_idf_hal::delay::{FreeRtos, BLOCK};
+use esp_idf_hal::gpio::*;
+use esp_idf_hal::i2c::*;
+use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_hal::prelude::*;
+use ssd1306::{ prelude::*, I2CDisplayInterface, Ssd1306};
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    text::{Baseline, Text},
+};
+
+const SSD1306_ADDRESS: u8 = 0x3c;
+
+fn main()-> anyhow::Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_sys::link_patches();
+
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
     info!("Hello, world - 3!");
+
+    let peripherals = Peripherals::take().unwrap();
+
+    let i2c = peripherals.i2c0;
+    let sda = peripherals.pins.gpio21;
+    let scl = peripherals.pins.gpio22;
+
+    let config = I2cConfig::new().baudrate(100.kHz().into());
+    let mut i2c = I2cDriver::new(i2c, sda, scl, &config)?;
+
+    let interface = I2CDisplayInterface::new(i2c);
+    let mut display = Ssd1306::new(
+        interface, DisplaySize128x64, DisplayRotation::Rotate0
+    ).into_terminal_mode();
+
+    display.init().unwrap();
+    // let _ = display.clear();
+
+    let text_style = MonoTextStyleBuilder::new()
+        .font(&FONT_6X10)
+        .text_color(BinaryColor::On)
+        .build();
+
+    let mut led = PinDriver::output(peripherals.pins.gpio14)?;
+
+    // Pins 35,34,39,26
+    let button = PinDriver::input(peripherals.pins.gpio35)?;
+
+    led.set_low()?;
+
+    Text::with_baseline("Hello world!", Point::zero(), text_style, Baseline::Top)
+        .draw(&mut display)
+        .unwrap();
+    display.flush().unwrap();
+
+    loop {
+        if button.is_high() {
+            led.set_high()?;
+        } else {
+            led.set_low()?;
+        }
+
+        // we are sleeping here to make sure the watchdog isn't triggered
+        FreeRtos::delay_ms(10);
+    }
 }
